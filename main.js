94ee7d3871a6cabb2952334c535c40f8c0ede447
Laksh Kataria <95770319+LakshK7@users.noreply.github.com>
2023-06-15 01:02:04 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import "./style.css";
 
-// import * as THREE from "three";
+import * as THREE from "three";
 
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
